cd91176eaf8c740f3e0605bf8f2cf42bcbe4ce82
Ivan Meshcheryakov <meshcheryakovvrn@gmail.com>
2021-02-26 17:00:06 +0300

    config for heroku

--- config.inc.php
+++ config.inc.php
@@ -1,15 +1,28 @@
 <?php
 class Main_config {
-    static $host = 'localhost';
-    // static $user = 'editus';
-    static $user = 'root';
-    static $password = '';
-    // static $password = 'eeX9dei';
-    static $db = 'editus';
+    // HEROKU SETTINGS
+    static $host = 'td5l74lo6615qq42.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'; //for heroku
+    static $user = 'bdshelr3xhih4wf5';
+    static $password = 'bx224esyzinr1pmz';
+    static $db = 's6cje90yvkgya233';
     static $sessiondir = '';
     static $main_file_name = 'editus.php';
     static $debug = 0;
     static $debugmail = 'debug-editus@banuchka.ru';
     static $serviceoff = 0;
+
+
+    
+    // static $host = 'localhost';
+    // static $user = 'editus';
+    // static $user = 'root';
+    // static $password = '';
+    // static $password = 'eeX9dei';
+    // static $db = 'editus';
+    // static $sessiondir = '';
+    // static $main_file_name = 'editus.php';
+    // static $debug = 0;
+    // static $debugmail = 'debug-editus@banuchka.ru';
+    // static $serviceoff = 0;
 }
 ?>
